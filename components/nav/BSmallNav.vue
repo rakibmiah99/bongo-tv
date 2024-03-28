0683ca53efffe321b6589c6460accb05a740e12c
rakibmiah99 <rakibmiah120199@gmail.com>
2024-03-28 23:22:48 +0600

    dd

--- components/nav/BSmallNav.vue
+++ components/nav/BSmallNav.vue
@@ -1,3 +1,55 @@
+<script setup lang="ts">
+const isOpen = ref(true)
+const links = [
+  [
+  {
+    label: 'Home',
+    to: '/',
+    icon: ''
+  },
+  {
+    label: 'Movies',
+    to: '/',
+    icon: ''
+  }, {
+    label: 'Series',
+    to: '/',
+    icon: ''
+  }, {
+    label: 'Kits',
+    to: '/',
+    icon: ''
+  }], [{
+    label: 'Examples',
+    icon: 'i-heroicons-light-bulb'
+  }, {
+    label: 'Help',
+    icon: 'i-heroicons-question-mark-circle'
+  },
+    {
+      label: 'Profile',
+      avatar: {
+        src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+      },
+      badge: 100
+    }
+  ]
+]
+
+
+const ui = {
+  wrapper: 'relative',
+  after: '',
+  base:'hover:text-purple-950 cursor-pointer uppercase ',
+  before: '',
+  divider: {
+    wrapper: {
+      base: 'p-2',
+    },
+  },
+}
+</script>
+
 <template>
   <UContainer>
     <div class="mt-5 bg-white rounded-lg py-4 px-8">
@@ -10,11 +62,20 @@
           </h1>
         </div>
         <!--COLLAPSE-->
+        <UButton icon="i-heroicons-bars-3" color="indigo" @click="isOpen = true"/>
       </div>
     </div>
   </UContainer>
+
+
+
+  <USlideover  side="right" :ui="{width: 'max-w-[200px]'}" v-model="isOpen">
+    <div class="p-4 flex-1">
+      <UVerticalNavigation :ui="ui" :links="links" >
+        <template #default="{ link }">
+          <span class="hover:text-indigo-950 relative">{{ link.label }}</span>
+        </template>
+      </UVerticalNavigation>
+    </div>
+  </USlideover>
 </template>
-<script setup lang="ts">
-import BAccount from "~/components/nav/BAccount.vue";
-import Menu from "~/components/nav/BMenu.vue";
-</script>
