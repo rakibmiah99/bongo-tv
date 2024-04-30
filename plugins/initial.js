3702b603a130745fedc73370abb79d5f044ba6ba
rakibmiah99 <rakibmiah120199@gmail.com>
2024-04-30 09:10:10 +0600

    asdfj

--- plugins/initial.js
+++ plugins/initial.js
@@ -1,15 +1,15 @@
-import {getMenus} from "~/composables/api.js";
+// import {getMenus} from "~/composables/api.js";
 
 export default defineNuxtPlugin({
     name: 'initial',
-    setup(nuxtApp){
-        const config = useRuntimeConfig();
-        return {
-            provide: {
-                base_url : config.public.BASE_URL
-            }
-        }
-    },
+    // setup(nuxtApp){
+    //     const config = useRuntimeConfig();
+    //     return {
+    //         provide: {
+    //             base_url : config.public.BASE_URL
+    //         }
+    //     }
+    // },
     // hooks: {
     //     async 'app:created'(){
     //         await getMenus();
