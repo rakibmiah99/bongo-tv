34c9cfe1a7c6a65d12fefc9968b80c1212ecb2f4
rakibmiah99 <rakibmiah120199@gmail.com>
2024-04-30 08:43:14 +0600

    test index

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,3 @@
 <template>
-
-  <BHeader/>
-
-  <CategoryWisedSectionMove :data="category" v-for="category in categoryWiseMovieStore"/>
-
-  <div class="py-8">
-    <BWatched/>
-  </div>
-
-  <Footer/>
+  <h1>Hello World</h1>
 </template>
-
-<script setup>
-import Footer from "~/components/BFooter.vue";
-import {useCategoryWiseMovie} from "~/composables/useCategoryWiseMovie.js";
-import {getHomepageData} from "~/composables/api.js";
-import BFixedNav from "~/components/nav/BFixedNav.vue";
-definePageMeta({
-  middleware: 'auth'
-})
-const {data, getProviders} = useAuth();
-console.log('providers', await getProviders())
-console.log('data', data.value)
-const {signOut} = useAuth();
-// await signOut();
-
-const {categoryWiseMovieStore, makeCategoryWiseMovie} = useCategoryWiseMovie();
-
-onMounted(() => getHomepageData())
-
-</script>
